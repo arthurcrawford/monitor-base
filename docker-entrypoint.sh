4d2fd0138e7d9fff5bf84b83c7efb3190b1db3ec
arthur.crawford <arthur.crawford@webexpenses.com>
2015-09-11 13:06:41 +0100

    Removed debug from entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,4 @@ set -e
 sed -i "s/set *smtp-auth-user.*/set smtp-auth-user=${SMTP_AUTH_USER}/g" /etc/mail.rc
 sed -i "s/set *smtp-auth-password.*/set smtp-auth-password=${SMTP_AUTH_PASSWORD}/g" /etc/mail.rc
 sed -i "s/.*icinga@localhost.*/  email = \"${ICINGA_ADMIN_EMAIL}\"/g" /etc/icinga2/conf.d/users.conf
-cat /etc/mail.rc
 exec "$@"
